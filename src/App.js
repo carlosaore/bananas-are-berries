650e64fb6c44dbcc6ea15005e396c050fc617b11
xuleasca <florea.roxana5@yahoo.com>
2021-02-09 10:33:37 +0000

    context template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
 
-export default App;
+import HomePage from "./components/home/Homepage";
+import PageOne from "./components/page1/Page1";
+import PageTwo from "./components/page2/Page2";
+import PageThree from "./components/page3/Page3";
+import PageFour from "./components/page4/Page4";
+
+const App = () => (
+  /* NOTE. IF YOU WANT TO CHANGE THE LINK: CHANGE IT HERE AND INSIDE LINK IN NAVBAR.JS */
+  <Switch>
+    <Route exact path="/">
+      <HomePage />
+    </Route>
+    <Route exact path="/page1">
+      <PageOne />
+    </Route>
+    <Route exact path="/page2">
+      <PageTwo />
+    </Route>
+    <Route exact path="/page3">
+      <PageThree />
+    </Route>
+    <Route exact path="/page4">
+      <PageFour />
+    </Route>
+  </Switch>
+);
+
+export default App
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,4 @@
+
+const Footer = () => <h1>I AM FOOTER</h1>;
+
+export default Footer;
