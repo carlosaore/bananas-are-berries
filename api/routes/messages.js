deb11601b9bce1d6a95e77ada6e2185b21da2c40
AyaDesigner <aya.berdyeva@gmail.com>
2021-02-11 14:30:35 +0100

    fixing

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -3,16 +3,26 @@ const router = express.Router();
 const Message = require('../models/messages');
 /* GET messages */
 router.get('/', function (req, res, next) {
-  Message.find({}, function (err, docs) {
+  Message.find({}     , function (err, docs) {
     res.send(docs);
   })
 });
+
+
+// await Message.find({ name: 'john', age: { $gte: 18 } }).exec();
+
+
 router.post('/', function (req, res, next) {
+
+
+
+
   const message = new Message({
     senderNumber: req.body.senderNumber,
     recepientNumber: req.body.recepientNumber,
-    status: req.body.status,
     message: req.body.message,
+
+    status: 'NOT_SENT',
     chiefId: req.body.chiefId
   });
   console.log(message);
