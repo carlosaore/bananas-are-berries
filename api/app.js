6ca8cf7fd094686a7dddd3279940ab20d095187b
xuleasca <florea.roxana5@yahoo.com>
2021-02-11 22:45:26 +0000

    post request front end

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const messagesRouter = require('./routes/messages');
@@ -33,6 +34,7 @@ mongoose.connect(process.env.DB_CONNECTION, {
 
 
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
