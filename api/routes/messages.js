cc7bdc3d32569b24f220eb08f02773aaf8264387
AyaDesigner <aya.berdyeva@gmail.com>
2021-02-11 20:33:33 +0100

    added get route

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -3,30 +3,25 @@ const router = express.Router();
 const Message = require('../models/messages');
 /* GET messages */
 router.get('/', function (req, res, next) {
-  Message.find({}, function (err, docs) {
-    res.send(docs);
-  })
+    Message.find({status: 'NOT_SENT'}, function (err, docs) {
+        res.send(docs);
+    })
 });
 
 
-// await Message.find({ name: 'john', age: { $gte: 18 } }).exec();
 
 
 router.post('/', function (req, res, next) {
 
-
-
-
-  const message = new Message({
-    senderNumber: req.body.senderNumber,
-    recepientNumber: req.body.recepientNumber,
-    message: req.body.message,
-
-    status: 'NOT_SENT',
-    chiefId: req.body.chiefId
-  });
-  console.log(message);
-  message.save();
-  res.send('working');
+    const message = new Message({
+        senderNumber: req.body.senderNumber,
+        recepientNumber: req.body.recepientNumber,
+        message: req.body.message,
+        status: 'NOT_SENT'
+        // chiefId: req.body.chiefId
+    });
+    console.log(message);
+    message.save();
+    res.send('working');
 });
 module.exports = router;
