b4ec4fc4c338d6d3f66febf80df1ca53273c555d
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2021-02-12 10:09:24 +0000

    caesar -7 Qr code

--- client/src/components/page2/decode-qr/DecodeQr.js
+++ client/src/components/page2/decode-qr/DecodeQr.js
@@ -19,6 +19,24 @@ const DecodeQr = () => {
         console.log(err)
       }
 
+      const caesar = function(word, num) {
+        let solved = ""
+        num = (num%26 + 26) % 26;
+        for (let i = 0; i < word.length ; i++) {
+            let ascii = word[i].charCodeAt();
+            if ((ascii >= 65 && ascii <= 90)) {
+                solved += String.fromCharCode((ascii - 'A'.charCodeAt(0) + num)%26 
+                + 'A'.charCodeAt(0)) ;
+            } else if(ascii >= 97 && ascii <= 122){
+                solved += String.fromCharCode((ascii-'a'.charCodeAt(0) + num) % 26 
+                + 'a'.charCodeAt(0));
+            } else {
+                solved += word[i]
+            }
+        }
+        return solved;
+    }
+
     // const onScanQr = () => {
     //     qrRef.current.openImageDialog();
     // }
@@ -45,7 +63,7 @@ const DecodeQr = () => {
             onScan={handleScan}
             style={{ width: '100%' }}
             // legacyMode
-            /> :  <p>{scanResultQr}</p>} </p>
+            /> :  <p>{caesar(scanResultQr, -7)}</p>} </p>
         </div>
         </StyledDecodeQrForm>
     )
